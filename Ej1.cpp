976aafc8178b63f415a8086626e804d46f48a768
ismael-2306347 <2306347@ucc.edu.ar>
2024-08-19 12:40:40 -0300

    ej1 y ej2

--- Ej1.cpp
+++ Ej1.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
+
 using namespace std;
 
+#include "lista/lista.h"
+
+void printlista(Lista<int> &lista, int p) {
+    int tamanio = lista.getTamanio();
+    if (p >= tamanio || p < 0) {
+        cout << "La posicion " << p << "esta fuera de los limites de la lista " << endl;
+        return;
+    }
+    for (int i = p; i < tamanio; ++i) {
+        cout << lista.getDato(i) << "->";
+    }
+    cout << "NULL" << endl;
+
+}
+
 int main() {
+    Lista<int> lista1;
+    int n, d, p;
+    cout<<"ingrese el tamaño de la lista1: ";
+    cin>>n;
 
-    cout << "Ejercicio N° 1" << endl;
+    for (int i = 0; i < n; ++i) {
+        cout<<"ingrese el valor de la lista en la posicion: "<<endl;
+        cin>>d;
+        lista1.insertarUltimo(d);
+    }
 
-    return 0;
+    cout<<"ingrese la posicion desde donde desea imprimir la lista1" <<endl;
+    cin>>p;
+
+    printlista(lista1,p);
+
+
 }
--- Ej2.cpp
+++ Ej2.cpp
@@ -1,9 +1,41 @@
+/*Implementar una función que recibe una lista de números enteros y un número entero “n” y
+que modifique la lista borrando todos los elementos de la lista que tengan el número “n”.
+Imprimir la lista antes de hacer el llamado a la función y luego del llamado a la función para
+mostrar que la lista original cambió. ej: lista: 1->2->3->4->5->3->7->8->3->10 con n=3 debe
+pasar con la función a 1->2->4->5->7->8.*/
 #include <iostream>
+
 using namespace std;
 
+#include "lista/lista.h"
+
+void printlista(Lista<int> &lista, int p) {
+    int tamanio = lista.getTamanio();
+    if (p >= tamanio || p < 0) {
+        cout << "La posicion " << p << "esta fuera de los limites de la lista " << endl;
+        return;
+    }
+    for (int i = p; i < tamanio; ++i) {
+        cout << lista.getDato(i) << "->";
+    }
+    cout << "NULL" << endl;
+
+}
+
 int main() {
+    Lista<int> lista1;
+    int n, d, p;
+    cout << "ingrese el tamaño de la lista1: ";
+    cin >> n;
 
-    cout << "Ejercicio N° 2" << endl;
+    for (int i = 0; i < n; ++i) {
+        cout << "ingrese el valor de la lista en la posicion: " << endl;
+        cin >> d;
+        lista1.insertarUltimo(d);
+    }
 
-    return 0;
+    cout << "ingrese la posicion desde donde desea imprimir la lista1" << endl;
+    cin >> p;
+
+    printlista(lista1, p);
 }
