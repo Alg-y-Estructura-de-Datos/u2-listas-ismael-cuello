9b3552d906cdae30f8c3bc5ebcd16c3725b34167
ismael-2306347 <2306347@ucc.edu.ar>
2024-08-24 12:06:45 -0300

    ej3

--- Ej3.cpp
+++ Ej3.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "Lista/Lista.h"
 using namespace std;
-
+void intercambiarlistas(Lista<int>&lista1,Lista<int>&lista2){
+    Lista<int> aux1,aux21;
+}
 int main() {
 
     cout << "Ejercicio N° 3" << endl;
